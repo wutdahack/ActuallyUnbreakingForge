ba5beb289cfc13ed8fb3a590218d618418303afa
WUT DA HACK <70644502+WUTDAHACK@users.noreply.github.com>
2021-03-02 10:40:01 +0000

    probably going to fix incompatibilities

--- src/main/java/wutdahack/actuallyunbreaking/mixin/UnbreakingEnchantmentMixin.java
+++ src/main/java/wutdahack/actuallyunbreaking/mixin/UnbreakingEnchantmentMixin.java
@@ -3,19 +3,22 @@ package wutdahack.actuallyunbreaking.mixin;
 import net.minecraft.enchantment.UnbreakingEnchantment;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(UnbreakingEnchantment.class)
 public class UnbreakingEnchantmentMixin {
 
-    // makes the unbreakabing enchantment actually make the tool unbreakable
-     @Overwrite
-     public static boolean negateDamage(ItemStack item, int level, Random rand) {
-         if (item.isDamageable() || item.isDamaged()) {
-             item.setDamage(-2147483648);
-         }
-         return true;
-     }
+    // when preventing damage, sets the tool to be actually unbreakable
+    @Inject(method = "negateDamage", cancellable = true, at = @At(value = "TAIL"))
+    // (learnt my lesson and i'm not using overwrite ever again lol)
+    private static void preventDamage(ItemStack item, int level, Random random, CallbackInfoReturnable<Boolean> cir) {
+        if (item.isDamageable() || item.isDamaged()) {
+            item.setDamage(-2147483648); // setting the item to be unbreakable with this ridiculous negative number
+        }
+        cir.setReturnValue(true); // this did the same thing overwrite did! :D
+    }
 }
